1c45d7d14c8a8faeff4ec352f28f9875698949b3
ilana99 <ilanagastou@gmail.com>
2026-05-01 16:51:56 +0200

    added jwt and logging

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,17 +2,25 @@ import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
 import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(loginDto: LoginDto) {
+  async login(loginDto: LoginDto): Promise<string> {
     const user = await this.userService.findOneByEmail(loginDto.email);
-    if (user != null) {
-      const result = await bcrypt.compare(loginDto.password, user.password);
-      if (result == true) return;
+    if (user == null) {
+      throw new Error();
     }
-    throw new Error();
+    const result = await bcrypt.compare(loginDto.password, user.password);
+    if (result == true) {
+      const payload = { sub: user.id, email: user.email, role: user.role };
+      return this.jwtService.signAsync(payload);
+    }
+    throw new Error('');
   }
 }
